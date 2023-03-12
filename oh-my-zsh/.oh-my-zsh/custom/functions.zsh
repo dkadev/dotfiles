b009a6db1e69c8ea509f39f02fc1090b9e90960c
dkadev <davidcomeron19@gmail.com>
2023-03-12 20:12:50 +0100

    add strip_pdf function

--- oh-my-zsh/.oh-my-zsh/custom/functions.zsh
+++ oh-my-zsh/.oh-my-zsh/custom/functions.zsh
@@ -33,3 +33,30 @@ function extractPorts(){
     echo -e "[*] Ports copied to clipboard\n"  >> extractPorts.tmp
     cat extractPorts.tmp; rm extractPorts.tmp
 }
+
+strip_pdf() {
+    echo "Original Metadata for $1"
+    exiftool $1
+
+    echo "Removing Metadata...."
+    echo ""
+    qpdf --linearize $1 stripped1-$1
+    exiftool -all:all= stripped1-$1
+    qpdf --linearize stripped1-$1 stripped2-$1
+    rm stripped1-$1
+    rm stripped1-$1_original
+
+    echo "New Metadata for stripped2-$1"
+    exiftool stripped2-$1
+    echo ""
+
+    echo "Securing stripped2-$1...."
+    password=$(pwgen -s 12 1)
+    echo "Password will be: $password"
+    echo ""
+    qpdf --linearize --encrypt $password $password 128 --print=full --modify=none --extract=n --use-aes=y -- stripped2-$1 stripped-$1
+    rm stripped2-$1
+
+    echo "Final status of stripped-$1"
+    pdfinfo -upw $password stripped-$1
+}
