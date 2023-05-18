99227253b8f09887b21e463a1cbeaef7d7f21ff4
dkadev <davidcomeron19@gmail.com>
2023-05-18 10:16:58 +0200

    extractPortsFormFile function

--- oh-my-zsh/.oh-my-zsh/custom/functions.zsh
+++ oh-my-zsh/.oh-my-zsh/custom/functions.zsh
@@ -22,16 +22,29 @@ function mkpt(){
     fi
 }
 
-# Extract nmap information
-function extractPorts(){
-    ports="$(cat $1 | grep -oP '\d{1,5}/open' | awk '{print $1}' FS='/' | xargs | tr ' ' ',')"
-    ip_address="$(cat $1 | grep -oP '\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}' | sort -u | head -n 1)"
-    echo -e "\n[*] Extracting information...\n" > extractPorts.tmp
-    echo -e "\t[*] IP Address: $ip_address"  >> extractPorts.tmp
-    echo -e "\t[*] Open ports: $ports\n"  >> extractPorts.tmp
-    echo $ports | tr -d '\n' | xclip -sel clip
-    echo -e "[*] Ports copied to clipboard\n"  >> extractPorts.tmp
-    cat extractPorts.tmp; rm extractPorts.tmp
+# Extract Nmap ports
+function extractPorts () {
+    ports="$(echo "$1" | grep -oP '\d{1,5}/open' | awk '{print $1}' FS='/' | xargs | tr ' ' ',')"
+    ip_address="$(echo "$1" | grep -oP '\d{1,3}.\d{1,3}.\d{1,3}.\d{1,3}' | sort -u | head -n 1)"
+    echo -e "\t[] IP Address: $ip_address" >> extractPorts.tmp
+    echo -e "\t[] Open ports: $ports\n" >> extractPorts.tmp
+    cat extractPorts.tmp
+    rm extractPorts.tmp
+}
+
+# Extract Nmap ports from file
+function extractPortsFromFile() {
+    if [ $# -eq 0 ]; then
+        echo "Usage: $0 <input_file>"
+        exit 1
+    fi
+    
+    input_file="$1"
+    output_file="output.txt"
+    
+    while read -r line; do
+        extractPorts "$line" >> "$output_file"
+    done < <(cat "$input_file")
 }
 
 # Strip and encrypt PDF
