1608f2c5d78668ac71a81ad55c5ad78ac3b9a185
dkadev <23488064+dkadev@users.noreply.github.com>
2023-05-24 20:26:17 +0200

    commented base16shell

--- oh-my-zsh/.oh-my-zsh/custom/base16.zsh
+++ oh-my-zsh/.oh-my-zsh/custom/base16.zsh
@@ -1,9 +1,9 @@
 # Base16 Shell
-BASE16_SHELL="$HOME/.config/base16-shell/"
-[ -n "$PS1" ] && \
-    [ -s "$BASE16_SHELL/profile_helper.sh" ] && \
-        source "$BASE16_SHELL/profile_helper.sh"
+# BASE16_SHELL="$HOME/.config/base16-shell/"
+# [ -n "$PS1" ] && \
+#     [ -s "$BASE16_SHELL/profile_helper.sh" ] && \
+#         source "$BASE16_SHELL/profile_helper.sh"
         
-base16_pico
+# base16_pico
 # base16_pop
 # base16_horizon-terminal-dark
