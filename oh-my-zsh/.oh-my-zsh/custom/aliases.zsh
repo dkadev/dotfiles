ec2c7811277b4262d0afd206d722c8aaa7207977
dkadev <davidcomeron19@gmail.com>
2024-07-11 21:32:58 +0200

    rollback batcat aliases

--- oh-my-zsh/.oh-my-zsh/custom/aliases.zsh
+++ oh-my-zsh/.oh-my-zsh/custom/aliases.zsh
@@ -5,7 +5,15 @@
 alias ls="lsd"
 alias ll="ls -lh"
 alias lla="ls -lah"
-alias cat="bat"
+# macOS
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  alias cat="bat"
+fi
+
+# Linux
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+  alias cat="batcat"
+fi
 alias ip="ip --color=auto"
 alias h="history"
 alias hs="history | grep"
