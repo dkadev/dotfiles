f00ded874151752cb06c4abb4917a05ea4ee0fd1
dkadev <23488064+dkadev@users.noreply.github.com>
2025-08-09 23:11:33 +0200

    refactor aliases to use eza for enhanced file listing functionality

--- oh-my-zsh/.oh-my-zsh/custom/aliases.zsh
+++ oh-my-zsh/.oh-my-zsh/custom/aliases.zsh
@@ -2,9 +2,14 @@
 # Aliases
 #------------------------------------------------------
 
-alias ls="lsd"
-alias ll="ls -lh"
-alias lla="ls -lah"
+alias ls='eza $eza_params'
+alias l='eza --git-ignore $eza_params'
+alias ll='eza --all --header --long $eza_params'
+alias llm='eza --all --header --long --sort=modified $eza_params'
+alias la='eza -lbhHigUmuSa'
+alias lx='eza -lbhHigUmuSa@'
+alias lt='eza --tree $eza_params'
+alias tree='eza --tree $eza_params'
 
 alias ff="fzf --style full --preview 'fzf-preview.sh {}' --bind 'focus:transform-header:file --brief {}'"
 
